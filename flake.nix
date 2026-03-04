d6ea8e4aa6e34ea14aedd0df6ad4f605d2f5bd2a
Your Name <you@example.com>
2026-03-04 22:17:18 +0100

    initialisation

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Pythonpoets nix files";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25-11";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    rec {
+      nixosConfigurations = {
+        zuerich =
+          nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./machines/zuerich/configuration.nix
+            ];
+          };
+      };
+    };
+}
---
+++ machines/zuerich/default.nix
@@ -0,0 +1,66 @@
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "zurich"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
+
+  users.users.david = {
+    isNormalUser = true;
+    description = "david";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+   openssh.authorizedKeys.keys = [
+      (builtins.readFile (builtins.fetchurl {
+        url = "https://github.com/pythonpoet.keys";
+        sha256 = "";
+      }))
+   ];
+  };
+   users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOr7hdJO0P2TBs5GH+XmOi7XoBT6LiAS7Ym6IEgM2H0 david@alpakapro"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqJMYvhMOn3ckmfn524NfrCPkdbtdXJZXfiy6hzQn22 david@alpakabook"
+    ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+   git
+   helix
+   wget
+   curl
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
