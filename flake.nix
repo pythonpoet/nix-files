84f1488d83b9b11b9f45abfc6a22a85c84ab8efe
Your Name <you@example.com>
2026-03-06 17:17:52 +0100

    Add tonda rename server to chuchichaestli

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   outputs = { self, nixpkgs, home-manager }:
     {
       nixosConfigurations = {
-        zuerich =
+        chuchichaestli =
           nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
