ecc7af19abb5e3ecab56bf8a94f564a95ababd2f
Your Name <you@example.com>
2026-03-14 12:37:00 +0100

    cloudflared

--- modules/cloudflared.nix
+++ modules/cloudflared.nix
@@ -2,7 +2,7 @@
     services.cloudflared = {
       enable = true;
       tunnels = {
-      "chuchichaestli_tunnel" = {
+      "8e979e98-e5fe-4d52-9112-ad4b2f10b955" = {
         credentialsFile = "/home/david/.cloudflared/8e979e98-e5fe-4d52-9112-ad4b2f10b955.json";
         default = "http_status:404";
       };
