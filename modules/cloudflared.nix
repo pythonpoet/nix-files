950f6ed07e6b9c42b73e9b2a5b2f5fe4d96c3f50
Your Name <you@example.com>
2026-03-14 12:29:01 +0100

    add nginx

--- modules/cloudflared.nix
+++ modules/cloudflared.nix
@@ -3,7 +3,7 @@
       enable = true;
       tunnels = {
       "chuchichaestli_tunnel" = {
-        credentialsFile = "/home/david/.cloudflared/cert.pem";
+        credentialsFile = "/home/david/.cloudflared/8e979e98-e5fe-4d52-9112-ad4b2f10b955.json";
         default = "http_status:404";
       };
       };
