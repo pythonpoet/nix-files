4c97d06499d46c548e12e3f0426b6f2a2a476315
Your Name <you@example.com>
2026-03-14 18:35:53 +0100

    add https

--- modules/cloudflared.nix
+++ modules/cloudflared.nix
@@ -6,7 +6,7 @@
         credentialsFile = "/home/david/.cloudflared/8e979e98-e5fe-4d52-9112-ad4b2f10b955.json";
         default = "http_status:404";
         ingress = {
-          "taaltaak.org" = "http://localhost:80";
+          "taaltaak.org" = "https://localhost:443";
         };
       };
       };
