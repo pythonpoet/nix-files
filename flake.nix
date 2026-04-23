3cf4309111c51d1a7fdcea99b6ba1c490c9ead25
Your Name <you@example.com>
2026-04-23 16:18:16 +0200

    adding postgres db

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
                     user = "taalbubbl";
                     host = "127.0.0.1";
                     port = 5432;
-                  }
+                  };
                 };
                 # authentik = {
                 #   enable = false;
