78b14ac8d9329f544ed71b640a78bdba37c2b799
Your Name <you@example.com>
2026-04-26 14:02:33 +0200

    add secrets to taalbubbl

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
                     host = "/run/postgresql";
                     port = 5432;
                   };
-                  environmentFile = config.age.secrets.taalbubbl.path;
+                  environmentFile = age.secrets.taalbubbl.path;
                 };
                 # authentik = {
                 #   enable = false;
