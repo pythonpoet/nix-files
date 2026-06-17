b3ed83d3dceaf50f438d3277705e606eaf56b309
Your Name <you@example.com>
2026-06-17 16:49:54 +0200

    librechat: enable mongoDB

--- modules/librechat.nix
+++ modules/librechat.nix
@@ -48,7 +48,7 @@ in {
       enable = true;
       env.PORT = cfg.port;
       credentialsFile = config.sops.secrets.requesty-token.path;
-      services.librechat.enableLocalDB = true;
+      librechat.enableLocalDB = true;
       settings = {
         cache = true;
         endpoints = {
