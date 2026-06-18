95b439352f970c36418867bc6a54d4331d13a469
Your Name <you@example.com>
2026-06-18 19:17:03 +0200

    update librechat settings yaml file

--- modules/librechat.nix
+++ modules/librechat.nix
@@ -29,20 +29,20 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.nginx = {
-      virtualHosts."${cfg.url}" = {
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:${toString cfg.port}";
-            proxyWebsockets = true;
-            extraConfig = ''
-              client_max_body_size 5000M;
-              proxy_read_timeout   600s;
-              proxy_send_timeout   600s;
-              send_timeout         600s;
-            '';
-          };
-      };
-    };
+    # services.nginx = {
+    #   virtualHosts."${cfg.url}" = {
+    #       locations."/" = {
+    #         proxyPass = "http://127.0.0.1:${toString cfg.port}";
+    #         proxyWebsockets = true;
+    #         extraConfig = ''
+    #           client_max_body_size 5000M;
+    #           proxy_read_timeout   600s;
+    #           proxy_send_timeout   600s;
+    #           send_timeout         600s;
+    #         '';
+    #       };
+    #   };
+    # };
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
       "mongodb"
       "mongodb-ce"
